171af08a4d20503a0b679464dd14cba8978dc704
Brian Merchant <bhmerchant@gmail.com>
2022-11-05 13:42:04 -0700

    init

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,35 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use derive_more::{Add, Sub};
+
+#[derive(Debug, Eq, PartialEq, Default, Clone, Copy, Add, Sub)]
+pub struct Point {
+    x: i32,
+    y: i32,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
+pub struct Rect {
+    pub p0: Point,
+    pub p1: Point,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[derive(Debug, Eq, PartialEq, Clone, Copy)]
+pub struct LayerRect {
+    pub rect: Rect,
+    pub layer: u8,
+}
+
+pub struct Polygon {
+    pub points: Vec<Point>,
+}
+
+pub struct RectilinearPolygon {
+    pub points: Vec<Point>,
+    pub vertical_walls: Vec<Point>,
+}
+
+impl Polygon {
+    pub fn count_inversions(&self) ->  {
+        
     }
 }
+
